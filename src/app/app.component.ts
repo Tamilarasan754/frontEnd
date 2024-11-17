efc939ab18ba3c3ff4fa935bfffb3e5f3527ea7b
Tamilarasan <tamilvlp754@gmail.com>
2024-11-17 11:21:18 +0530

    feat:code added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+ 
 })
 export class AppComponent {
   title = 'Test1';
--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,12 +3,16 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   imports: [
     AppModule,
-    ServerModule,
+    ServerModule
+
   ],
+  providers:[],
   bootstrap: [AppComponent],
 })
 export class AppServerModule {}
